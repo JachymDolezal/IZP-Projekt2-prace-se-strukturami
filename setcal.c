207532f4b6be5202ab6ca169b9a874bb9ff2fa34
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-11-10 14:06:29 +0100

    Add files via upload

---
+++ setcal.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool argument_check(){
+//todo
+return 0;
+}
+
+void file_opener(){
+//todo
+}
+
+void my_malloc(){
+//todo
+}
+
+bool file_check(){
+//todo
+}
+
+void line_parser(){
+//todo
+}
+
+// N funkci pro operaci kde je output struktura
+void func1_N(){
+//todo
+}
+
+// N funkci kde je vystup bool hodnota
+bool func_bool(){
+//todo
+}
+
+//prints output of a bool function
+void bool_print(){
+//todo
+}
+
+//outputs relation
+void relation_print(){
+//todo
+}
+
+// outputs set
+void set_print(){
+//todo
+}
+
+typedef struct {
+// nejak zakomponovat struktury do relací/mnozin
+// pole indexu univerza??
+int test;
+} Struktura;
+
+int main(int argc, char **argv){
+    printf("num of arguments %d, arguments %s",argc,argv[0]);
+
+    /*
+    ----Steps programu-----
+
+    1. kontrola argumentu
+    2. nacteni gitsouboru
+    3. malloc pro obsah souboru
+    4. kontrola souboru
+    5. ulozeni typu:
+        - univerzum
+        - mnoziny/relace
+        - funkce
+    6. provedení funkcí na množinách/relacích
+    7. prislusny tisk vysledku funkce
+    8. ukončení programu
+
+    */
+
+    return 0;
+}
