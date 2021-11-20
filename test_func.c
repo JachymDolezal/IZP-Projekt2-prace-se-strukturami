d5b963d9ea10bb4d2c67fba3c3f65f87154b8a68
your_uncle <xpolia05@vutbr.cz>
2021-11-20 19:59:26 +0100

    doplneni funkci 3 5 6 9 do test_func.c

--- test_func.c
+++ test_func.c
@@ -14,12 +14,30 @@ void set_print(int size, int *array){
 // [5,0,4]
 }
 
-//prints true if set is empty, otherwise prints false.
+//1. prints true if set is empty, otherwise prints false.
 bool is_empty(int cardinality){
     return cardinality>0 ? false : true;
 }
 
-//Outputs a union of two sets.
+//2. ...
+
+//3. outputs complement of a set(in relation with universe)
+void do_complement(char *universe, int universe_cardinality, int *set, int set_cardinality){
+    bool found;
+    
+    printf("S ");
+    for (int i = 0; i < universe_cardinality; i++){
+        found = false;
+        for (int j = 0; j < set_cardinality; j++){
+            if (set[j] == i)
+                found = true;
+        }
+        if (!found)
+            printf("%s ", universe[i]);
+    }
+}
+
+//4.. Outputs a union of two sets.
 void do_union(int *array_a, int cardinality_A, int * array_b, int cardinality_B){
     bool in_union;
     int *union_set = malloc((cardinality_A+cardinality_B)*sizeof(int)); //creates a temp array of a maximum union of the two arrays.
@@ -48,27 +66,81 @@ void do_union(int *array_a, int cardinality_A, int * array_b, int cardinality_B)
 
 }
 
-//returns true if set A is subset of set B, otherwise returns false.
+//5. ...
+
+//6. prints A-B
+void minus(int *array_A, int cardinality_A, int *array_B, int cardinality_B){
+    //todo, ak sa element z A nenachadza v B, mozem ho printit... that is all folks
+    // dost sa mi algoritmus podoba na subseteq forsomreason to asi je to iste haha, ak !found tak printim a done
+    bool element_found;
+    int difference = malloc(cardinality_A*sizeof(int));
+    int diff_idx = 0;
+    
+    for (int i = 0; i < cardinality_A; i++){
+        element_found = false;
+        for (int j = 0; j < cardinality_B; j++){
+            if(array_A[i] == array_B[j]){
+                element_found = true;
+                break;
+            }
+            if (!element_found)
+                difference[diff_idx] = array_A[i];
+                diff_idx++;
+        }
+    }
+    set_print(diff_idx+1, difference);
+    free(difference);
+}
+
+//7. returns true if set A is subset of set B, otherwise returns false.
 //REWORK algorithm
 bool subseteq(int *array_A, int cardinality_A, int *array_B, int cardinality_B){
-    // [1,2]
+    // [3,4]
     // [1,2,3,4,5,6,7]
+    bool element_found;
     for(int i = 0; i < cardinality_A; i++){
+        element_found = false;
         for(int j = 0; j < cardinality_B; j++){
             if(array_A[i] == array_B[j]){
                 //element in set was found in set B
-
+                element_found = true;
+                break;
             }
-            else{
-                //element was not found in set B. Hence, set A is not a subset of set B.
-                return false;
-            }
+            // else{
+            //     //element was not found in set B. Hence, set A is not a subset of set B.
+            //     return false;
+        }
+        if (!element_found){
+            return false;
         }
     }
     return true;
 }
 
+//8. returns true if set A is vlastna subset of set B
+bool subset(int *array_A, int cardinality_A, int *array_B, int cardinality_B){
+    if (subseteq(array_A, cardinality_A, array_B, cardinality_B) == true && cardinality_A < cardinality_B)
+        return true;
+    return false;
+}
 
+//9. returns true if set A is equal to set B
+int compare(const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+bool equals(int *array_A, int cardinality_A, int *array_B, int cardinality_B){
+//https://www.cplusplus.com/reference/cstring/memcmp/
+//https://www.cplusplus.com/reference/cstdlib/qsort/
+    if (cardinality_A != cardinality_B)
+        return false;
+
+    qsort(array_A, cardinality_A, sizeof(int), compare); //tato funkce zmeni hodnoty v Set *s!!! snad to neni problem...
+    qsort(array_B, cardinality_B, sizeof(int), compare);
+    
+    if (memcmp(array_A, array_B, cardinality_A*sizeof(int)) == 0)
+        return true;
+    return false;
+}
 
 
 int main(int argc, char *argv[]){
